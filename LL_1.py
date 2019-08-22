f90c8751b895f2d5ef302571e6fb31d28aad57e0
Daniel Castro <daniel.7c.n12@gmail.com>
2019-08-22 19:21:01 +0100

    Refactored transverse method, added tests

--- LL_1.py
+++ LL_1.py
@@ -4,7 +4,14 @@ class LinkedList:
         self.next = None
 
     def transverse(self):
-        while(self != None):
-            self = self.next
+        node = self
+        while(node != None):
+            print(f'value = {node.value}')
+            node = node.next
 
-            
+
+ll = LinkedList(0)
+ll.next = LinkedList(1)
+ll.next.next = LinkedList(2)
+
+ll.transverse()
